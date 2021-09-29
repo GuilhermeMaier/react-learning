b5bcc8caf2050117b7ae43338a1c13d1727c5ef4
Guilherme Maier <maier_gui_11@hotmail.com>
2021-09-29 00:42:35 -0300

    reajuste apenas para voltar a buildar

--- src/pages/time.tsx
+++ src/pages/time.tsx
@@ -1,21 +1,62 @@
+// import React from "react";
+// import Link from "next/dist/client/link";
+// import moment from "moment";
+// import axios from "axios";
+
+// async function Time(props) {
+//   const config = {
+//     url: "https://www.maier-dev.com.br/api/time",
+//   };
+
+//   const dynamicDataResponse = await axios(config)
+//     .then(function (response) {
+//       return response.data;
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+
+//   console.log("Response:", dynamicDataResponse);
+//   const dynamicData = dynamicDataResponse.dynamicDate;
+//   console.log("Data final:", dynamicData);
+//   console.log("Type Data final:", typeof dynamicData);
+//   return (
+//     <div>
+//       <h1>Tempo</h1>
+//       <Link href="/">
+//         <a>Index</a>
+//       </Link>
+//       <div>{dynamicData} (dinâmico)</div>
+//       <div>{props.staticDate} (estático)</div>
+//     </div>
+//   );
+// }
+
+// export function getStaticProps() {
+//   const staticDate = moment().format("DD/MM/YYYY HH:mm:ss");
+//   return {
+//     props: {
+//       staticDate,
+//     },
+//     revalidate: 60 * 1,
+//   };
+// }
+
+// export default Time;
+
 import React from "react";
 import Link from "next/dist/client/link";
 import moment from "moment";
 
-async function Time(props) {
-  const dynamicDataResponse = await fetch("https://maier-dev.com.br/api/time");
-  console.log("Response:", dynamicDataResponse);
-  const dynamicDataJson = await dynamicDataResponse.json();
-  console.log("Response em JSON:", dynamicDataJson);
-  const dynamicData = dynamicDataJson.dynamicDate;
-  console.log("Data final:", dynamicData);
+function Time(props) {
+  const dynamicDate = moment().format("DD/MM/YYYY HH:mm:ss");
   return (
     <div>
       <h1>Tempo</h1>
       <Link href="/">
         <a>Index</a>
       </Link>
-      <div>{dynamicData} (dinâmico)</div>
+      <div>{dynamicDate} (dinâmico)</div>
       <div>{props.staticDate} (estático)</div>
     </div>
   );
