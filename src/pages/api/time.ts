8d98fef33b633b6fc8a278febed9998bfddd8e97
Guilherme Maier <maier_gui_11@hotmail.com>
2021-09-28 22:32:46 -0300

    ajuste não funcional :'(

--- src/pages/api/time.ts
+++ src/pages/api/time.ts
@@ -1,7 +1,7 @@
 import moment from "moment";
 
 function Time(request, response) {
-  const dynamicDate = moment();
+  const dynamicDate = moment().format("DD/MM/YYYY HH:mm:ss");
   const node = process.env.ENVIRONMENT;
 
   response.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
--- src/pages/time.tsx
+++ src/pages/time.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import Link from "next/dist/client/link";
 import moment from "moment";
 
-function Time(props) {
-  const dynamicDate = moment().format("DD/MM/YYYY HH:mm:ss");
+async function Time(props) {
+  const dynamicDataResponse = await fetch("https://maier-dev.com.br/api/time");
+  console.log("Response:", dynamicDataResponse);
+  const dynamicDataJson = await dynamicDataResponse.json();
+  console.log("Response em JSON:", dynamicDataJson);
+  const dynamicData = dynamicDataJson.dynamicDate;
+  console.log("Data final:", dynamicData);
   return (
     <div>
       <h1>Tempo</h1>
       <Link href="/">
         <a>Index</a>
       </Link>
-      <div>{dynamicDate} (dinâmico)</div>
+      <div>{dynamicData} (dinâmico)</div>
       <div>{props.staticDate} (estático)</div>
     </div>
   );
