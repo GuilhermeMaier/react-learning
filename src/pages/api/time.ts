50ad8827d1697994e2bc568e1e448ae2c12c6805
Guilherme Maier <maier_gui_11@hotmail.com>
2021-09-28 21:04:04 -0300

    mesmo endpoint com stale while revalidate

--- src/pages/api/time.ts
+++ src/pages/api/time.ts
@@ -4,6 +4,8 @@ function Time(request, response) {
   const dynamicDate = moment();
   const node = process.env.ENVIRONMENT;
 
+  response.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
+
   response.json({
     dynamicDate,
     node,
