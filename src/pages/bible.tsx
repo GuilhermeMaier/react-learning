aecff021b193200cdd590498523d207a22aa4f24
Guilherme Maier <maier_gui_11@hotmail.com>
2021-09-30 23:48:20 -0300

    readmissão de bíblia

--- src/pages/bible.tsx
+++ src/pages/bible.tsx
@@ -1,46 +1,30 @@
-// import React from "react";
-// import Link from "next/link";
-// import axios from "axios";
-
-// async function getText(bookAbbrv: string, chapter: number, verse: number) {
-//   try {
-//     return await axios.post(`/api/bible/${bookAbbrv}`, {
-//       chapter: chapter,
-//       verse: verse,
-//     });
-//   } catch (error) {
-//     throw new Error(error);
-//   }
-// }
-
-// function Bible() {
-//   return (
-//     <div>
-//       <h1>Sobre</h1>
-//       <Link href="/">
-//         <a>Index</a>
-//       </Link>
-//       <input placeholder={"Capítulo"}></input>
-//       <input placeholder={"Versículo"}></input>
-//       <button onClick={}></button>
-//     </div>
-//   );
-// }
-
-// export default Bible;
-
 import React from "react";
 import Link from "next/link";
+import axios from "axios";
 
-function Fon() {
+async function getText(bookAbbrv: string, chapter: number, verse: number) {
+  try {
+    return await axios.post(`/api/bible/${bookAbbrv}`, {
+      chapter: chapter,
+      verse: verse,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+function Bible() {
   return (
     <div>
       <h1>Sobre</h1>
       <Link href="/">
         <a>Index</a>
       </Link>
+      <input placeholder={"Capítulo"}></input>
+      <input placeholder={"Versículo"}></input>
+      <button onClick={}></button>
     </div>
   );
 }
 
-export default Fon;
+export default Bible;
