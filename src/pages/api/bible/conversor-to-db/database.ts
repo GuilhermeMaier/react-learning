99762bbe617410d8388c3c40538746423c2c3682
Guilherme Maier <maier_gui_11@hotmail.com>
2021-10-02 16:21:15 -0300

    .

--- src/pages/api/bible/conversor-to-db/database.ts
+++ src/pages/api/bible/conversor-to-db/database.ts
@@ -15,16 +15,17 @@ async function connectDatabase(uri: string) {
 }
 
 async function insertBookOnDB(DBBook: DBVerse[]): Promise<boolean> {
-  const db = await connectDatabase(process.env.MONGODB_URI);
-  const collection = db.collection("bible");
-  try {
-    for (const currentVerse of DBBook) {
-      await collection.insertOne(currentVerse);
-    }
-    return true;
-  } catch (error) {
-    throw new Error(error);
-  }
+  return true;
+  // const db = await connectDatabase(process.env.MONGODB_URI);
+  // const collection = db.collection("bible");
+  // try {
+  //   for (const currentVerse of DBBook) {
+  //     await collection.insertOne(currentVerse);
+  //   }
+  //   return true;
+  // } catch (error) {
+  //   throw new Error(error);
+  // }
 }
 
 export default insertBookOnDB;
