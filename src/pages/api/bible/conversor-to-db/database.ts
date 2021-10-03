b746cfe04853888d28e8bb0a6ed1da20f45d2ae5
Guilherme Maier <maier_gui_11@hotmail.com>
2021-10-02 23:16:53 -0300

    ajustes para atualizar a descrição do versículo caso exista um registro com o mesmo nome, capítulo e versículo, senão insere no banco

--- src/pages/api/bible/conversor-to-db/database.ts
+++ src/pages/api/bible/conversor-to-db/database.ts
@@ -15,17 +15,28 @@ async function connectDatabase(uri: string) {
 }
 
 async function insertBookOnDB(DBBook: DBVerse[]): Promise<boolean> {
-  return true;
-  // const db = await connectDatabase(process.env.MONGODB_URI);
-  // const collection = db.collection("bible");
-  // try {
-  //   for (const currentVerse of DBBook) {
-  //     await collection.insertOne(currentVerse);
-  //   }
-  //   return true;
-  // } catch (error) {
-  //   throw new Error(error);
-  // }
+  const db = await connectDatabase(process.env.MONGODB_URI);
+  const collection = db.collection("bible");
+  try {
+    for (const currentVerse of DBBook) {
+      const currentDocument = await collection.findOneAndUpdate(
+        {
+          chapterId: currentVerse.chapterId,
+          verseId: currentVerse.verseId,
+          name: currentVerse.name,
+        },
+        {
+          $set: { verseDescription: currentVerse.verseDescription },
+        }
+      );
+      if (currentDocument.value === null) {
+        await collection.insertOne(currentVerse);
+      }
+    }
+    return true;
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
 export default insertBookOnDB;
