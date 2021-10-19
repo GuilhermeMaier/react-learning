4492b9efb91d9a1ea28b0fb6481d87599c9b99ca
Guilherme Maier <guilherme@enzo.com.br>
2021-10-19 14:39:35 -0300

    ajustes de estrutura

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,23 +1,2 @@
-import React from "react";
-import { SidebarProps } from "./dto/sidebar.dto";
-import {
-  SidebarContainer,
-  SidebarHeader,
-  SidebarMenuItem,
-  SidebarMenuItemsContainer,
-} from "./sidebar.styles";
-
-const Sidebar = (props: SidebarProps) => {
-  const { backgroundImage, sidebarHeader, sidebarMenuItems } = props;
-  const menuItemsJSX = sidebarMenuItems.map((item, index) => {
-    return <SidebarMenuItem key={index}>{item}</SidebarMenuItem>;
-  });
-  return (
-    <SidebarContainer backgroundImage={backgroundImage}>
-      <SidebarHeader>{sidebarHeader}</SidebarHeader>
-      <SidebarMenuItemsContainer>{menuItemsJSX}</SidebarMenuItemsContainer>
-    </SidebarContainer>
-  );
-};
-
-export default Sidebar;
+import SidebarComponent from "./sidebar";
+export default SidebarComponent;
