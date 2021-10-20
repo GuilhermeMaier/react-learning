62d719785d836ca042f8cb5c8e2f3502b10b6061
Guilherme Maier <maier_gui_11@hotmail.com>
2021-10-19 22:09:09 -0300

    teste

--- src/components/sidebar/sidebar.styles.ts
+++ src/components/sidebar/sidebar.styles.ts
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import * as celtic from "../../common/images/celtic-cross.jpg";
+import { SidebarContainerProps } from "./dto/sidebar.dto";
 
-export const SidebarContainer = styled.div`
+export const SidebarContainer = styled.div<SidebarContainerProps>`
   min-width: 80px;
   max-width: 280px;
   width: 20%;
@@ -11,8 +11,29 @@ export const SidebarContainer = styled.div`
       rgba(3, 177, 77, 0.9) 0%,
       rgba(4, 98, 29, 0.9) 74%
     ),
-    url("images/celtic3.jpg");
+    url(${(props) => props.backgroundImage});
   background-size: cover;
   background-repeat: no-repeat;
   background-position-x: center;
+  color: white;
+`;
+
+export const SidebarHeader = styled.h3`
+  padding: 20px 10px;
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+export const SidebarMenuItemsContainer = styled.div`
+  padding: 20px 10px;
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+export const SidebarMenuItem = styled.div`
+  padding: 6px 20px;
+  text-align: center;
+  font-weight: 600;
+  margin-bottom: 10px;
+  color: rgba(19, 15, 64);
 `;
